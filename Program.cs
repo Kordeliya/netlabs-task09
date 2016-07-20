68c76eebf775ac92a8b52d9fd4ee839f00cb27c7
Kordeliya <miss-neismit1991@yandex.ru>
2016-07-20 15:05:54 +0400

    Add Project

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zueva_Natalia_Expression
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Замена x+1 значения на инкремент
+                Console.WriteLine("Замена x+1 значения на инкремент");
+                Expression<Func<int, int, double>> expForVisit = (x, y) => (double)(x + 1) * (1 + y) / 2;
+                Console.WriteLine(expForVisit.ToString());
+                QueryTransformer transformer = new QueryTransformer();
+                Expression newexp = transformer.VisitAndConvert<Expression>(expForVisit, "TEST");
+                Console.WriteLine(newexp.ToString());
+                Console.WriteLine();
+
+
+                //составление формулы с помощью Expression выражений
+                ParameterExpression xParam = Expression.Parameter(typeof(int), "x");
+                ParameterExpression yParam = Expression.Parameter(typeof(int), "y");
+                ConstantExpression constant = Expression.Constant(2.0);
+
+                Expression sum = Expression.Add(xParam, yParam);
+                sum = Expression.Convert(sum, typeof(System.Double));
+                Expression divide = Expression.Divide(sum, constant);
+                LambdaExpression lambdaExpression = Expression.Lambda(divide, xParam, yParam);
+                var newLambda = (Func<int, int, double>)lambdaExpression.Compile();
+                Console.WriteLine("составление формулы с помощью Expression выражений. {0}", lambdaExpression.ToString());
+                Console.WriteLine("Результат: newLambda(10, 1)= {0}", newLambda(10, 1));
+                Console.WriteLine();
+
+
+                //составление Expression с помощью лямбда выражения
+                Expression<Func<double, double, double>> exp = (x, y) => (x + y) / 2.0;
+                var del = exp.Compile();
+                Console.WriteLine("составление Expression с помощью лямбда выржанения. {0}", exp.ToString());
+                Console.WriteLine("Результат: del(10, 1) = {0}", del(10, 1));
+                Console.WriteLine();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Произошла ошибка");
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+   
+}
---
+++ QueryTransformer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zueva_Natalia_Expression
+{
+    /// <summary>
+    /// Класс для трансформации
+    /// </summary>
+    public class QueryTransformer : ExpressionVisitor
+    {
+        /// <summary>
+        /// При просмотре бинарного выражения
+        /// </summary>
+        /// <param name="node">выражение</param>
+        /// <returns>измененное выражение</returns>
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            Expression newLeft = node.Left;
+            Expression newRight = node.Right;
+
+            bool IsRightTypeLeft = node.Left is ConstantExpression || node.Left is ParameterExpression;
+            bool IsRightTypeRight = node.Right is ConstantExpression || node.Right is ParameterExpression;
+            if (!IsRightTypeLeft)
+                newLeft = Visit(node.Left);
+            if (!IsRightTypeRight)
+                newRight = Visit(node.Right);
+
+
+            if (node.NodeType == ExpressionType.Add)
+            {
+                ConstantExpression constant = null;
+                ParameterExpression param = null;
+                if (newLeft is ConstantExpression)
+                    constant = (ConstantExpression)newLeft;
+                else if (newRight is ConstantExpression)
+                    constant = (ConstantExpression)newRight;
+                if (newLeft is ParameterExpression)
+                    param = (ParameterExpression)newLeft;
+                else if (newRight is ParameterExpression)
+                    param = (ParameterExpression)newRight;
+
+                if ((int)constant.Value == 1 && param != null)
+                {
+                    return Expression.PreIncrementAssign(param);
+                }
+            }
+
+            return node.Update(newLeft, null, newRight);
+
+        }
+    }
+}
